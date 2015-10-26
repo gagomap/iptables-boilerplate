1e676bc7b5c05e68147880f8328aa44333173dab
gagomap <hiepdangvan.iv@gmail.com>
2015-10-27 00:42:08 +0700

    Update

--- etc/firewall/custom/iptablesrules.sh
+++ etc/firewall/custom/iptablesrules.sh
@@ -70,10 +70,13 @@ $IPTABLES -A INPUT -p tcp --syn --dport 22 -m connlimit --connlimit-above 10 -j 
 ####################
 
 # Clone nginx spam and install ipset
+echo " clone nginx spam"
 
 \curl -sSL https://raw.githubusercontent.com/gagomap/nginx_blacklists/master/autoblock.sh > /etc/cron.daily/autoblock.sh
 chmod +x /etc/cron.daily/autoblock.sh
 sh /etc/cron.daily/autoblock.sh
+
+echo "install ipset"
 
 \curl -sSL https://raw.githubusercontent.com/gagomap/install_ipset/master/installUbuntu14_04.sh > /usr/local/bin/installUbuntu14_04.sh
 chmod +x /usr/local/bin/installUbuntu14_04.sh
@@ -82,6 +85,7 @@ sh /usr/local/bin/installUbuntu14_04.sh
 ####################
 
 # Clone fail2ban settings
+echo "update fail2ban settings"
 
 \curl -sSL https://raw.githubusercontent.com/gagomap/install_fail2ban/master/install_fail2ban.sh > /usr/local/bin/install_fail2ban.sh
 chmod +x /usr/local/bin/install_fail2ban.sh
