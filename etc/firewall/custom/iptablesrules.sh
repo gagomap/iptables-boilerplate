32d4e8909ef4c5797d868ab77c160ee7099d620c
gagomap <hiepdangvan.iv@gmail.com>
2015-11-02 17:25:06 +0700

    Update

--- etc/firewall/custom/iptablesrules.sh
+++ etc/firewall/custom/iptablesrules.sh
@@ -1,46 +1,43 @@
 #!/bin/bash
+echo "Limit the amount of NEW connections " 
 
 ## https://github.com/bmaeser/iptables-boilerplate/tree/master/custom-examples
-## Limit the amount of NEW connections 
 ## to a maximum of $CONNECTIONS per $SECONDS per remote-ip
 ## this is usefull, if someone tries to DOS or synflood your box
 ## and helps to prevent dictonary-attacks
-
 ## hint: if you get messages like "xt_recent: hitcount (120) is larger than packets to be remembered (20)"
 ## the xt_recent kernel module (called ipt_recent on some systems) is set to remeber only 20 connections
 ## see: https://github.com/bmaeser/iptables-boilerplate/issues/1#issuecomment-8935056
-
 
 ## we allow at max 120 new connections per minute
 #CONNECTIONS=120 or 20
 #SECONDS=60
 
-
 IPTABLES=/sbin/iptables
 IP6TABLES=/sbin/ip6tables
 
-$IPTABLES -A INPUT -p tcp -m conntrack --ctstate NEW -m recent --set
-$IPTABLES -A INPUT -p tcp -m conntrack --ctstate NEW -m recent --update --seconds 5 --hitcount 20 -j DROP
+# We disable it, bescause ddos-deflate is better.
+# $IPTABLES -A INPUT -p tcp -m conntrack --ctstate NEW -m recent --set
+# $IPTABLES -A INPUT -p tcp -m conntrack --ctstate NEW -m recent --update --seconds 2 --hitcount 20 -j DROP
 
 echo "############################################################"
-####################
+
+# /bin/bash
+
+echo "Limit the amount of connections on per remote-ip"
+
+# CONNECTIONS=30
+
+# IPTABLES=/sbin/iptables
+
+# We disable it, bescause ddos-deflate is better.
+# $IPTABLES -A INPUT -p tcp --syn -m connlimit --connlimit-above 60 -j REJECT
+
 echo "############################################################"
+
 #/bin/bash
 
-## Limit the amount of connections on per remote-ip
-
-#CONNECTIONS=30
-
-#IPTABLES=/sbin/iptables
-
-$IPTABLES -A INPUT -p tcp --syn -m connlimit --connlimit-above 30 -j REJECT
-
-echo "############################################################"
-#####################
-echo "############################################################"
-#/bin/bash
-
-## Limit the amount of NEW connections on port 22
+echo "Limit the amount of NEW connections on port 22"
 ## to a maximum of $CONNECTIONS per $SECONDS per remote-ip
 ## this is usefull, if someone tries to DOS or synflood your box
 ## and helps to prevent dictonary-attacks
@@ -59,64 +56,54 @@ $IPTABLES -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --set
 $IPTABLES -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
 
 echo "############################################################"
-#####################
-echo "############################################################"
+
 #/bin/bash
 
-## Limit the amount of connections on port 22 per remote-ip
+echo "Limit the amount of connections on port 22 per remote-ip"
 
 ## this rule does NOT open port 22. it just drops "too many connections" on port 22
 
-#CONNECTIONS=5
+# CONNECTIONS=5
 
-#IPTABLES=/sbin/iptables
+# IPTABLES=/sbin/iptables
 
 $IPTABLES -A INPUT -p tcp --syn --dport 22 -m connlimit --connlimit-above 10 -j REJECT
 
 echo "############################################################"
-##################################################################
-echo "############################################################"
 
-# Clone nginx spam and install ipset
-
-echo "############################################################"
-echo "clone nginx spam"
-echo "############################################################"
+echo "Clone nginx spam and install ipset"
 
 \curl -sSL https://raw.githubusercontent.com/gagomap/nginx_blacklists/master/autoblock.sh > /etc/cron.daily/autoblock.sh
 chmod +x /etc/cron.daily/autoblock.sh
 
 echo "############################################################"
-echo "install ipset"
-echo "############################################################"
+
+echo "Install ipset"
 
 \curl -sSL https://raw.githubusercontent.com/gagomap/install_ipset/master/installUbuntu14_04.sh > /usr/local/bin/installUbuntu14_04.sh
 chmod +x /usr/local/bin/installUbuntu14_04.sh
 sh /usr/local/bin/installUbuntu14_04.sh
 
-####################
-
-# Clone fail2ban settings
 
 echo "############################################################"
-echo "update fail2ban settings"
-echo "############################################################"
+
+echo "Update fail2ban settings"
 
 \curl -sSL https://raw.githubusercontent.com/gagomap/install_fail2ban/master/install_fail2ban.sh > /usr/local/bin/install_fail2ban.sh
 chmod +x /usr/local/bin/install_fail2ban.sh
 sh /usr/local/bin/install_fail2ban.sh
 
-####################
-
-# Create Ipset blacklist
-
 echo "############################################################"
 echo "Create Ipset blacklist"
-echo "############################################################"
 
 $IPTABLES -I INPUT -m set --match-set blacklist src -j DROP
 
-####################
 
+echo "Start ddos-deflate service"
+
+service ddos start
+
+echo "############################################################"
 # save the current firewall config to be reapplied at restart
 # $IPTABLES-save | tee /etc/iptables.rules
+echo "############################################################"
